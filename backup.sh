80ecd0fd2add38a657887d16f2119070a299831c
Jonathan Price <pricetx@hotmail.com>
2014-08-22 09:00:11 +0100

    Previous commit had removed chunks of code - restored

--- backup.sh
+++ backup.sh
@@ -42,6 +42,82 @@ elif [ ! -r "${CRTFILE}" ]; then
         exit
 fi
 
+# Check that SSH login to remote server is successful
+if [ ! "$(ssh -p ${REMOTEPORT} ${REMOTEUSER}@${REMOTESERVER} echo test)" ]; then
+        log "Failed to login to ${REMOTEUSER}@${REMOTESERVER}"
+        log "Make sure that your public key is in their authorized_keys"
+        exit
+fi
+
+# Check that remote directory exists and is writeable
+if [ $(ssh -p "${REMOTEPORT}" "${REMOTEUSER}"@"${REMOTESERVER}" touch "${REMOTEDIR}"/test) ]; then
+        log "Failed to write to ${REMOTEDIR} on ${REMOTESERVER}"
+        log "Check file permissions and that ${REMOTEDIR} is correct"
+        exit
+else
+        # Remove the temporary file
+        ssh -p "${REMOTEPORT}" "${REMOTEUSER}"@"${REMOTESERVER}" rm "${REMOTEDIR}"/test
+fi
+
+BACKUPDATE=$(date -u +%Y-%m-%d-%H%M)
+STARTTIME=$(date +%s)
+TARFILE="${LOCALDIR}""$(hostname)"-"${BACKUPDATE}".tgz
+SQLFILE="${TEMPDIR}mysql_${BACKUPDATE}.sql"
+
+cd "${LOCALDIR}"
+
+### END OF CHECKS ###
+
+### MYSQL BACKUP ###
+
+if [ ! $(command -v mysqldump) ]; then
+        log "mysqldump not found, not backing up MySQL!"
+elif [ -z $ROOTMYSQL ]; then
+        log "MySQL root password not set, not backing up MySQL!"
+else
+        log "Starting MySQL dump dated ${BACKUPDATE}"
+        mysqldump -u root -p${ROOTMYSQL} --all-databases > ${SQLFILE}
+        log "MySQL dump complete"
+
+        #Add MySQL backup to BACKUP list
+        BACKUP=(${BACKUP[*]} ${SQLFILE})
+fi
+
+### END OF MYSQL BACKUP ###
+
+### TAR BACKUP ###
+
+log "Starting tar backup dated ${BACKUPDATE}"
+# Prepare tar command
+TARCMD="-zcf ${TARFILE} ${BACKUP[*]}"
+
+# Add exclusions to front of command
+for i in ${EXCLUDE[@]}; do
+        TARCMD="--exclude $i ${TARCMD}"
+done
+
+# Run tar
+tar ${TARCMD}
+
+# Encrypt tar file
+log "Encrypting backup"
+openssl smime -encrypt -aes256 -binary -in ${TARFILE} -out ${TARFILE}.enc -outform DER -stream ${CRTFILE}
+log "Encryption completed"
+
+BACKUPSIZE=$(du -h "${TARFILE}" | cut -f1)
+log "Tar backup complete. Filesize: ${BACKUPSIZE}"
+
+# Delete unencrypted tar
+rm "${TARFILE}"
+
+log "Tranferring tar backup to remote server"
+scp -P "${REMOTEPORT}" "${TARFILE}".enc "${REMOTEUSER}"@"${REMOTESERVER}":"${REMOTEDIR}"
+log "File transfer completed"
+
+if [ ! $(command -v mysqldump) ]; then
+        if [ ! -z ${ROOTMYSQL} ]; then
+                log "Deleting temporary MySQL backup"
+                rm ${SQLFILE}
         fi
 fi
 
